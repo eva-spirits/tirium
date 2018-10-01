b901ff2e85b81e1f24000b3eedcbe31111592734
Josip Horvat <josiphorvat000@gmail.com>
2018-10-01 17:36:18 +0200

    Initial Project Structure

---
+++ Source/Battle for Tirium/Assets/Scripts/CharacterInfo.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Unit", menuName = "Unit")]
+public class CharacterInfo : ScriptableObject
+{
+    public string unitName;
+    public int health;
+    public int damage;
+    public Mesh model;
+}
